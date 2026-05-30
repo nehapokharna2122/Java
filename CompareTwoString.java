c9b64837bebff0253038c5e2fe7189dece5ee581
nehapokharna2122 <nehapokharna2122@gmail.com>
2026-05-30 16:15:04 +0530

    Add files via upload

---
+++ CompareTwoString.java
@@ -0,0 +1,33 @@
+// WAP to Compare Two String. 
+
+package java_assignment;
+
+import java.util.Scanner;
+
+public class CompareTwoString 
+{
+	
+
+	    public static void main(String[] args) 
+	    {
+	        Scanner sc = new Scanner(System.in);
+
+	        System.out.print("Enter first string: ");
+	        String str1 = sc.nextLine();
+
+	        System.out.print("Enter second string: ");
+	        String str2 = sc.nextLine();
+
+	        if (str1.equals(str2))
+	        {
+	            System.out.println("Both strings are equal.");
+	        } 
+	        else 
+	        {
+	            System.out.println("Both strings are not equal.");
+	        }
+
+	        sc.close();
+	    }
+	}
+
