89b60dd6f26cb62d113f130272d58fc7e650dc34
nehapokharna2122 <nehapokharna2122@gmail.com>
2026-05-30 13:51:49 +0530

    Add files via upload

---
+++ NaturalNo.java
@@ -0,0 +1,16 @@
+package java_assignment;
+
+public class NaturalNo 
+{
+	    public static void main(String[] args) 
+	    {
+	        int i = 1;
+
+	        while(i <= 10)
+	        {
+	            System.out.println(i);
+	            i++;
+	        }
+	    }
+}
+
