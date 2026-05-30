9f496240c8ffead30c0a3d7c8379e6cbbef49d6c
nehapokharna2122 <nehapokharna2122@gmail.com>
2026-05-30 16:23:53 +0530

    Add files via upload

---
+++ ConcatenateString.java
@@ -0,0 +1,18 @@
+package java_assignment;
+
+public class ConcatenateString 
+{
+public static void main(String[] args) 
+    {
+        
+        String str1 = "Hello";
+        String str2 = " World";
+
+        String result = str1.concat(str2);
+
+        System.out.println("First String : " + str1);
+        System.out.println("Second String: " + str2);
+        System.out.println("Concatenated String: " + result);
+    }
+}
+
