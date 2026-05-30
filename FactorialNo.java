81c97bf3b1f47e0293dccc14a1c1a327a09e807b
nehapokharna2122 <nehapokharna2122@gmail.com>
2026-05-30 13:44:33 +0530

    Add files via upload

---
+++ FactorialNo.java
@@ -0,0 +1,26 @@
+package java_assignment;
+
+
+import java.util.Scanner;
+	public class FactorialNo 
+   {
+
+	 
+	    public static void main(String[] args) 
+	    {
+	        Scanner sc = new Scanner(System.in);
+
+	        System.out.print("Enter a number: ");
+	        int num = sc.nextInt();
+
+	        int fact = 1;
+
+	        for(int i = 1; i <= num; i++) 
+	        {
+	            fact = fact * i;
+	        }
+
+	        System.out.println("Factorial = " + fact);
+	    }
+	}
+
